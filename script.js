ede49c36fd1c6083f22867394b14e333ddd49693
LucasMeiraa <lucasmeira152@gmail.com>
2021-08-27 15:17:06 -0300

    menu e orçamento

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-
 document.querySelector(".hamburguer").addEventListener("click", () => 
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector("#qtde").addEventListener("change",atualizarPreco) //primeiro evento q quero executa e dps a funcao q vai ser executada
+document.querySelector("#js").addEventListener("change",atualizarPreco)
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1 
+
+    document.querySelector("#preco").innerHTML =  `R$ ${preco.toFixed(2)}`
+}    
